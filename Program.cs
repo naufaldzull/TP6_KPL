04c87e9648c97f5d9ba4b1bfbbd41b4bf3c0cae8
naufa <naufal.mdzulfik@gmail.com>
2025-03-23 21:44:06 +0700

    implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using tpmodul6_103022300021;
+using System.Diagnostics;
 
 class Program
 {
@@ -7,6 +8,24 @@ class Program
     {
         SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Dzulfikar");
         video.PrintVideoDetails();
+
+        try
+        {
+            video.IncreasePlayCount(10000000); // batas akhir
+            video.PrintVideoDetails();
+
+            for (int i = 0; i < 100; i++)
+            {
+                video.IncreasePlayCount(10000000); // Memicu overflow
+            }
+
+            video.PrintVideoDetails();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
     }
 }
 
--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace tpmodul6_103022300021
 {
@@ -14,17 +15,29 @@ namespace tpmodul6_103022300021
 
         public SayaTubeVideo(string title)
         {
+            Debug.Assert(title != null && title.Length <= 100, "Judul video tidak boleh null dan maksimal 100 karakter.");
+
             Random random = new Random();
             this.id = random.Next(10000, 99999);
             this.title = title;
             this.playCount = 0;
         }
-
         public void IncreasePlayCount(int count)
         {
-            playCount += count;
-        }
+            Debug.Assert(count > 0 && count <= 10000000, "Play count harus antara 1 - 10.000.000.");
 
+            try
+            {
+                checked
+                {
+                    playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("ERROR: Play count melebihi batas integer!");
+            }
+        }
         public void PrintVideoDetails()
         {
             Console.WriteLine($"ID: {id}");
